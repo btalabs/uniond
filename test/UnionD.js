2fb724449e80039661a10565c07a1cc1edb70820
hugooconnor <hugo.oconnor@gmail.com>
2016-11-14 16:29:02 +1100

    many votes test

--- test/UnionD.js
+++ test/UnionD.js
@@ -14,14 +14,14 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   });
 
-  it("should be able to get Member count", function(done) {
+  it("should get Member count", function(done) {
     let uniond = Uniond.deployed();
     uniond.getMemberCount.call({from: accounts[0]}).then((result) => {
       assert.equal(result, 1, 'member count not set to 1');
     }).then(done).catch(done);
   });
 
-  it("should be able to review active members", function(done) {
+  it("should review active members", function(done) {
     let uniond = Uniond.deployed();
     uniond.getMemberCount.call().then((result) => {
       return uniond.reviewActiveMembers(0, result.toNumber(), {from: accounts[0]});
@@ -32,7 +32,7 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   });
 
-  it("should be able to add new member", function(done) {
+  it("should add new member", function(done) {
     var uniond = Uniond.deployed();
     uniond.applyMember({from: accounts[1], value: 1000}).then((result) => {
       return uniond.addMember(accounts[1], {from: accounts[0]});
@@ -43,7 +43,7 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   });
 
-  it("should be able to create an issue", function(done) {
+  it("should create an issue", function(done) {
     let uniond = Uniond.deployed();
     uniond.addIssue("test").then((result) => {
       return uniond.vote(0, true, 1, {from: accounts[0]});
@@ -54,7 +54,7 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   })
 
-  it("should be able to transfer votes to a proxy", function(done) {
+  it("should transfer votes to a proxy", function(done) {
     let uniond = Uniond.deployed();
     uniond.transferVotes(accounts[0], 1, {from: accounts[1]}).then((result) => {
       return uniond.member(accounts[0]);
@@ -68,14 +68,14 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   })
 
-  it("should be able to get Uniond balance", function(done) {
+  it("should get Uniond balance", function(done) {
     let uniond = Uniond.deployed();
     uniond.unionBalance().then((result) => {
       assert.equal(result.toNumber(), 1000, "uniond balance not correct");
     }).then(done).catch(done);
   })
 
-  it("should be able to run a general election", function(done) {
+  it("should run a general election", function(done) {
     let uniond = Uniond.deployed();
     //elect accounts[3] to treasurer
     uniond.addElection(accounts[3], 1, {from: accounts[0]}).then((result) => {
@@ -97,7 +97,7 @@ contract('UnionD', function(accounts) {
     }).then(done).catch(done);
   })
 
-  it("should be able to pass an ammendment", function(done) {
+  it("should pass an ammendment", function(done) {
     let uniond = Uniond.deployed();
     uniond.newAmendment("test", 2, 55, {from: accounts[0]}).then((result) => {
       return uniond.voteAmendment(0, {from: accounts[0]});
@@ -109,6 +109,56 @@ contract('UnionD', function(accounts) {
       return uniond.constitution(2);
     }).then((result) => {
       assert.equal(result.toNumber(), 55, "amendment not passed");
+    }).then(done).catch(done);
+  })
+
+  it("should pass an election with many votes", function(done) {
+    let applyMember = (uniond, count) => {
+      promises = [];
+      for(let i = 0; i < count; i++){
+        promises.push(uniond.applyMember({from: accounts[i], value: 1000}));
+      }
+      return promises;
+    }
+
+    let addMember = (uniond, count) => {
+      promises = [];
+      for(let i = 0; i < count; i++){
+        promises.push(uniond.addMember(accounts[i], {from: accounts[0]}));
+      }
+      return promises;
+    }
+
+    let voteMany = (uniond, count, electionIndex) => {
+      promises = [];
+      for(let i = 0; i < count; i++){
+        promises.push(uniond.voteElection(electionIndex, {from: accounts[i]}));
+      }
+      return promises;
+    }
+
+    let uniond = Uniond.deployed();
+
+    Promise.all(applyMember(uniond, 100)).then((results) => {
+      return Promise.all(addMember(uniond, 100));
+    }).then((result) => {
+      return uniond.addElection(accounts[4], 2, {from: accounts[0]});
+    }).then((result) => {
+      return uniond.reviewActiveMembers(0, 100, {from: accounts[0]});
+    }).then((result) => {
+      return uniond.getMemberCount.call();
+    }).then((result) => {
+      return uniond.activeMembers.call();
+    }).then((result) => {
+      return Promise.all(voteMany(uniond, 56, 1));
+    }).then((result) => {
+      return uniond.elections.call(1);
+    }).then((result) => {
+      return uniond.executeElectionMandate(1);
+    }).then((result) => {
+      return uniond.member(accounts[4]);
+    }).then((result) => {
+      assert.equal(result[5], true, "accounts[4] not made a member admin")
     }).then(done).catch(done);
   })
 
